3e676e93713dca75f369bb4c0d948536fc59dc7c
okni-c <oknistay@gmail.com>
2021-11-21 02:23:10 -0600

    Added project files

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,54 @@
+// import the gql tagged template function
+const { gql } = require('apollo-server-express');
+
+// create our typeDefs
+const typeDefs = gql`
+    type Comment {
+        _id: ID
+        commentText: String
+        createdAt: String
+        username: String
+        reactionCount: Int
+        reactions: [Reaction]
+    }
+
+    type Reaction {
+        _id: ID
+        reactionBody: String
+        createdAt: String
+        username: String
+    }
+
+    type User {
+        _id: ID
+        username: String
+        email: String
+        friendCount: Int
+        comments: [Comment]
+        friends: [User]
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Query {
+        me: User
+        users: [User]
+        user(username: String!): User
+        comments(username: String): [Comment]
+        comment(_id: ID!): Comment
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        addComment(commentText: String!): Comment
+        addReaction(commentId: ID!, reactionBody: String!): Comment
+        addFriend(friendId: ID!): User
+    }
+`;
+
+// export the typeDefs
+module.exports = typeDefs;
